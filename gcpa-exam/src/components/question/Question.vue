33aa82d03b7a052a72819a5204e1bc14d100f341
Raphael Farias <raphael.f@outlook.com>
2022-01-12 14:51:32 -0300

    Adding Question and Answers component and getting the response selected by the user

--- gcpa-exam/src/components/question/Question.vue
+++ gcpa-exam/src/components/question/Question.vue
@@ -1,17 +1,54 @@
 <template>
-  <h2>{{ title }}</h2>
+  <v-card style="padding-left: 10px">
+    <div class="font-weight-bold question-title-padding">
+      {{ this.question.title }}
+    </div>
+    <v-list v-for="answer in this.question.questionOptionList"
+            :key="answer.optionDescription"
+            class="radio-button-padding-left"
+    >
+      <input type="radio"
+             :id="answer.optionDescription"
+             :value="answer"
+             v-text="answer.optionDescription"
+             v-model="selectedOption"
+             @change="onRadioButtonChange($event)"
+      >
+      <label for="answer.optionDescription"
+             class="d-inline-flex">
+        <Answer :option="answer.optionDescription"
+                :is-correct="answer.correct" />
+      </label>
+    </v-list>
+  </v-card>
 </template>
 
 <script>
+import Answer from "@/components/question/Answer";
 export default {
   name: "Question",
+  components: {
+    Answer
+  },
   props: {
-    title: String,
-
+    question: Object
+  },
+  data: () => ({
+    selectedOption: Object
+  }),
+  methods: {
+    onRadioButtonChange() {
+      this.$emit("selectedOption", this.selectedOption);
+    }
   }
 }
 </script>
 
 <style scoped>
-
+.question-title-padding {
+  padding: 10px;
+}
+.radio-button-padding-left {
+  padding-left: 10px;
+}
 </style>
