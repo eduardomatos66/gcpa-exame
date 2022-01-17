125a65b4a13ed27d39abafd623a637a2bfc42226
Eduardo Matos <eduardo.matos@priceline.com>
2022-01-17 11:55:08 -0300

    Add About

--- gcpa-exam/src/views/About.vue
+++ gcpa-exam/src/views/About.vue
@@ -1,5 +1,45 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<article class="markdown-body entry-content container-lg" itemprop="text">
+<h1 dir="auto">
+<a id="user-content-google-cloud-certified-associate---cloud-engineer-exam" class="anchor" aria-hidden="true" href="#google-cloud-certified-associate---cloud-engineer-exam">
+<path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path></a><a href="https://github.com/eduardomatos66/gcpa-exame">Google Cloud Certified Associate - Cloud Engineer exam </a></h1>
+<p dir="auto">The main source of study :</p>
+<ul dir="auto">
+<li><strong>Book</strong>: Official Google Cloud Certified Associate Cloud Engineer Study Guide</li>
+<li><strong>Google Clould Documentation</strong>: <a href="https://cloud.google.com/docs" rel="nofollow">https://cloud.google.com/docs</a></li>
+<li><strong>Whizlab Course</strong> - <a href="https://www.whizlabs.com/learn/course/google-cloud-certified-associate-cloud-engineer" rel="nofollow">google-cloud-certified-associate-cloud-engineer</a></li>
+</ul>
+<p dir="auto">Questions get from site:</p>
+<ul dir="auto">
+<li><a href="https://gcp-examquestions.com/gcp-associate-cloud-engineer-practice-exam-part-1/" rel="nofollow">gcp-associate-cloud-engineer-practice-exam-part-1</a></li>
+<li><a href="https://gcp-examquestions.com/gcp-associate-cloud-engineer-practice-exam-part-2/" rel="nofollow">gcp-associate-cloud-engineer-practice-exam-part-2</a></li>
+<li><a href="https://gcp-examquestions.com/gcp-associate-cloud-engineer-practice-exam-part-3/" rel="nofollow">gcp-associate-cloud-engineer-practice-exam-part-3</a></li>
+<li><a href="https://gcp-examquestions.com/gcp-associate-cloud-engineer-practice-exam-part-4/" rel="nofollow">gcp-associate-cloud-engineer-practice-exam-part-4</a></li>
+<li><a href="https://gcp-examquestions.com/gcp-associate-cloud-engineer-practice-exam-part-5/" rel="nofollow">gcp-associate-cloud-engineer-practice-exam-part-5</a></li>
+<li><a href="https://gcp-examquestions.com/gcp-associate-cloud-engineer-practice-exam-part-6/" rel="nofollow">gcp-associate-cloud-engineer-practice-exam-part-6</a></li>
+</ul>
+<p dir="auto">Questions location:</p>
+<ul dir="auto">
+<li>
+<p dir="auto">gcpa-exame\src\main\resources\questions</p>
+</li>
+<li>
+<p dir="auto">Question JSON format sample:</p>
+<p dir="auto">{
+<br>&emsp;"key": "001",
+<br>&emsp;"title": "1. You are a project owner and need your co-worker to deploy a new version of your
+application to App Engine. You want to follow Google's recommended practices.
+Which IAM roles should you grant your co-worker?",
+<br>&emsp;"alternatives": [
+<ul>
+<li>"A. Project Editor",</li>
+<li>"B. App Engine Service Admin",</li>
+<li>"C. App Engine Deployer",</li>
+<li>"D. App Engine Code Viewer"</li>
+</ul>&emsp;],
+<br>&emsp;"answer": "Answer: C - <a href="https://cloud.google.com/iam/docs/understanding-roles%22%7D" rel="nofollow">https://cloud.google.com/iam/docs/understanding-roles"
+<br>}</a></p>
+</li>
+</ul>
+</article>
 </template>
