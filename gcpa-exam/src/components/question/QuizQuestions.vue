08da37606567256fa56483e3745d38542520d833
Raphael Farias <raphael.f@outlook.com>
2022-01-18 20:25:57 -0300

    Adding new features to the GCPA Quiz screen - Added Next, Previous, and Finish exam buttons - Added the logic for these buttons - Fixed logic for scoring the answers as correct or not once now the user must click on Next/Previous to go to the next/previous question - Added a color to the selected answer. So, in case the user goes back and forth, the previously selected answer is highlighted - Fixed the field for selecting the number of questions to retrieve. Now the exam is starting with the selected amount of questions - Removed the question loading when the page starts loading. Only load the questions after clicking on "Start exam"

--- gcpa-exam/src/components/question/QuizQuestions.vue
+++ gcpa-exam/src/components/question/QuizQuestions.vue
@@ -19,7 +19,10 @@
             title="Google Cloud Platform Associate - Practice Exam"
             class="mb-2 gcpa-card">
           <b-button @click="startQuizFunc()">Start Exam </b-button>
-          <div style="margin-top: 20px"><p>Number of questions:</p> <input :value="this.numberOfQuestions" style="text-align: center; font-size: 20px;"/></div>
+          <div style="margin-top: 20px">
+            <p>Number of questions:</p>
+            <input v-model="numberOfQuestions" style="text-align: center; font-size: 20px;"/>
+          </div>
         </b-card>
       </span>
 
@@ -44,12 +47,24 @@
               height="8px"
           ></b-progress>
 
-          <div class="question-title">
+          <div class="question-title" v-if="questions.length">
             {{ questions[currentQuestion].title }}
           </div>
 
-          <div class="answer-section">
-            <b-button :key="index" v-for="(option, index) in questions[currentQuestion].questionOptionList" @click="handleAnswerClick(option.correct, index)" class="ans-option-btn" variant="primary">{{option.optionDescription}}</b-button>
+          <div class="answer-section" v-if="questions.length">
+            <b-button :key="index"
+                      v-for="(option, index) in questions[currentQuestion].questionOptionList"
+                      @click="handleAnswerClick(option.correct, index)"
+                      class="ans-option-btn"
+                      :class="{ 'selected-answer': questions[currentQuestion].chosen !== undefined && questions[currentQuestion].chosen === index }"
+                      variant="primary">
+                      {{ option.optionDescription }}
+            </b-button>
+          </div>
+          <div class="d-inline-flex">
+            <b-button :disabled="currentQuestion === 0" class="margin-right" @click="goToPreviousQuestion">Previous</b-button>
+            <b-button v-show="currentQuestion < questions.length - 1" @click="goToNextQuestion">Next</b-button>
+            <b-button v-show="currentQuestion === questions.length - 1" @click="finishExam">Finish exam</b-button>
           </div>
         </b-card>
       </span>
@@ -82,7 +97,6 @@ export default {
       try {
         const {data} = await QuestionService.getQuestions(this.numberOfQuestions)
         this.questions = data;
-        console.log(this.questions);
         this.countDown = 120 * this.questions.length;
       } catch (error) {
         console.log(error);
@@ -96,26 +110,48 @@ export default {
       this.countDownTimer()
     },
 
-    handleAnswerClick(isCorrect, choosed) {
+    handleAnswerClick(isCorrect, chosen) {
+      this.questions[this.currentQuestion]["chosen"] = chosen;
+      console.log(this.questions[this.currentQuestion]["chosen"])
+      this.questions[this.currentQuestion]["correctlyAnswered"] = isCorrect;
+    },
+
+    goToNextQuestion() {
       let nextQuestionNum = this.currentQuestion + 1;
-      this.questions[this.currentQuestion]["choosed"] = choosed;
-      if(isCorrect) {
-        this.score = this.score + 1;
-      }
 
       if(nextQuestionNum < this.questions.length) {
         this.currentQuestion = nextQuestionNum;
         // this.$store.state.questionAttended = this.currentQuestion;
         // localStorage.setItem('qattended', this.currentQuestion)
       }
-      else {
-        // localStorage.removeItem('qattended')
-        this.showScore = true;
-        this.lastCountDown = this.countDownDisplay;
-        clearTimeout(this.timer);
-        // localStorage.setItem('testComplete',this.showScore)
+    },
+
+    goToPreviousQuestion() {
+      let previousQuestion = this.currentQuestion - 1;
+
+      if(previousQuestion >= 0) {
+        this.currentQuestion = previousQuestion;
+        // this.$store.state.questionAttended = this.currentQuestion;
+        // localStorage.setItem('qattended', this.currentQuestion)
       }
     },
+
+    finishExam() {
+      // localStorage.removeItem('qattended')
+      let currentScore = 0;
+      this.showScore = true;
+      this.lastCountDown = this.countDownDisplay;
+      clearTimeout(this.timer);
+
+      this.questions.forEach(function (question) {
+        if(question.correctlyAnswered) {
+          currentScore++;
+        }
+      });
+      this.score = currentScore;
+      // localStorage.setItem('testComplete',this.showScore)
+    },
+
     countDownTimer() {
       if(this.countDown > 0) {
         this.timer = setTimeout(() => {
@@ -137,13 +173,9 @@ export default {
       }
     },
   },
+
   mounted: function() {
-    //  alert(this.$store.state.questionAttended)
-    //    this.showScore = localStorage.getItem('testComplete') || false
-    //    this.currentQuestion = localStorage.getItem('qattended') || 0
-    //    this.countDownTimer()
-    //    this.fetchQuiz()
-    this.getQuestions()
+
   }
 }
 </script>
@@ -205,6 +237,10 @@ export default {
   padding: 10px 10px 10px 5%;
 }
 
+.selected-answer {
+  background-color: #007bfe;
+}
+
 .question-counter {
   font-size: 14px;
   text-align: left;
@@ -218,5 +254,9 @@ export default {
 .quiz-card {
   padding-left: 15%;
   padding-right: 15%;
+}
+
+.margin-right {
+  margin-right: 20px;
 }
 </style>
--- gcpa-exam/src/components/question/Report.vue
+++ gcpa-exam/src/components/question/Report.vue
@@ -1,12 +1,12 @@
 <template>
     <v-card>
-        <div :key="index" v-for="(quest, index) in questions" class="question-padding-left">
-            <div class="question-title">{{ quest.title }}</div>
+        <div :key="index" v-for="(question, index) in questions" class="question-padding-left">
+            <div class="question-title">{{ question.title }}</div>
 
-            <b-button :key="index2" v-for="(option, index2) in quest.questionOptionList" class="ans-option-btn" 
-            v-bind:style="option.correct ? {backgroundColor: '#076b00'} : quest.choosed === index2  ? {backgroundColor: '#bd2600'} : {backgroundColor: '#6a84e3'}" variant="primary">{{option.optionDescription}}</b-button>
+            <b-button :key="index2" v-for="(option, index2) in question.questionOptionList" class="ans-option-btn"
+            v-bind:style="option.correct ? {backgroundColor: '#076b00'} : question.chosen === index2  ? {backgroundColor: '#bd2600'} : {backgroundColor: '#6a84e3'}" variant="primary">{{option.optionDescription}}</b-button>
 
-            <div class="explanation-text"> {{quest.explanation}} </div>
+            <div class="explanation-text"> {{question.explanation}} </div>
         </div>
     </v-card>
 </template>
