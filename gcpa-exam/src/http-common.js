6a3439aed2a5a0a39e6d0a940445ae2992d3e6a3
Eduardo Matos <eduardo.matos@priceline.com>
2022-02-02 21:30:00 -0300

    Back to env variables

--- gcpa-exam/src/http-common.js
+++ gcpa-exam/src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 function getBaseURL() {
     const path = "question";
-    let baseURL = process.env.VUE_APP_ROOT_API + ":8000/{path}".replace("{path}", path);
+    let baseURL = "http://localhost:8000/{path}".replace("{path}", path);
     if (process.env.VUE_APP_ENVIRONMENT === "production") {
         baseURL = "http://{address}:{port}/{path}".replace("{address}", process.env.VUE_APP_ADDRESS)
                                                   .replace("{port}", process.env.VUE_APP_PORT)
